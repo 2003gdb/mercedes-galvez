71bdb27d2388e9385f5144666c3c161593f47b69
2003gdb <2003gdb@gmail.com>
2025-05-18 12:41:55 -0600

    bigger on mobile hero-logo.png

--- components/landing/HeroSection.jsx
+++ components/landing/HeroSection.jsx
@@ -4,15 +4,14 @@ import Image from "next/image";
 export default function HeroSection() {
   return (
     <div className="z-10 w-full min-h-screen flex flex-col items-center justify-center px-4 text-center relative overflow-hidden">
-      <div className="w-full max-w-2xl md:max-w-2xl lg:max-w-3xl relative aspect-square">
-        <Image
-          src="/logo-hero.PNG"
-          alt="Mercedes Logo"
-          fill
-          className="object-contain"
-          priority
-        />
-      </div>
+      <Image
+        src="/logo-hero.PNG"
+        alt="Mercedes Logo"
+        width={1200}
+        height={1200}
+        className="w-[150vw] sm:w-[80vw] max-w-none sm:max-w-[600px] object-contain z-10"
+        priority
+      />
       <Card className="w-full max-w-md mx-4 bg-white/10 backdrop-blur-xs border-none absolute top-1/2 transform -translate-y-1/2 z-20 p-3 md:p-5">
         <CardContent className="flex flex-col items-center p-4 px-2">
           <h1 className="text-3xl tracking-tight" style={{ fontFamily: 'var(--font-lexend-mega)' }} >Terapia Electromagnética</h1>
